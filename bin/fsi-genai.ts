b8161b2147898ccb21dd6522f4f94846ed4babf4
Cyrus TC <cyrus.tc@gmail.com>
2024-03-18 00:46:27 +0800

    run prettier & fix syntax

--- bin/fsi-genai.ts
+++ bin/fsi-genai.ts
@@ -6,9 +6,9 @@ import { FsiGenaiDemoStack } from "../lib/fsi-genai-demo-stack";
 
 const app = new cdk.App();
 new FsiGenaiDemoStack(app, "FsiGenaiDemoStack", {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: "us-west-2" },
+  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: "us-west-2" },
 });
 
 new BrIamStack(app, "BrIamStack", {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: "us-west-2" },
+  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: "us-west-2" },
 });
